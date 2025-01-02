55e58416f5ed73782a616b31515cedd4b17bedaf
TriNHM <triminh0502@gmail.com>
2025-01-02 14:32:57 +0700

    [UPDATE] fix Fe src

---
+++ FEProject/src/utils/axiosInstance.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://localhost:7027/apiapi", // Base URL of backend
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+// Mock function to refresh token locally
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const user = JSON.parse(localStorage.getItem("loggedInUser"));
+    const token = localStorage.getItem("token");
+    const expirationTime = localStorage.getItem("expirationTime");
+
+    // Kiểm tra nếu không phải đang ở trang login và có token
+    if (config.url !== "/login" && token) {
+      if (new Date().getTime() > expirationTime) {
+        localStorage.clear();
+        if (
+          window.confirm("Your session has expired. Click OK to log in again.")
+        ) {
+          window.location.href = "/login"; // Chuyển hướng người dùng về trang login
+        }
+      } else {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
