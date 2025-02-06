570b82899c2c7e8f5c8b39bef05715f1ed5784c2
TriNHM <triminh0502@gmail.com>
2025-02-06 16:27:35 +0700

    [UPDATE] fix Shop component

--- FEProject/src/pages/Shop/Shop.js
+++ FEProject/src/pages/Shop/Shop.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
 import Pagination from "../../components/pageProps/shopPage/Pagination";
-import ProductBanner from "../../components/pageProps/shopPage/ProductBanner";
 import ShopSideNav from "../../components/pageProps/shopPage/ShopSideNav";
 
 const Shop = () => {
   const [itemsPerPage, setItemsPerPage] = useState(12);
-  const itemsPerPageFromBanner = (itemsPerPage) => {
-    setItemsPerPage(itemsPerPage);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedBrand, setSelectedBrand] = useState(null);
+
+  const handleCategorySelect = (categoryId) => {
+    setSelectedCategory(categoryId);
+  };
+
+  const handleBrandSelect = (brandId) => {
+    setSelectedBrand(brandId);
   };
 
   return (
@@ -16,11 +22,18 @@ const Shop = () => {
       {/* ================= Products Start here =================== */}
       <div className="w-full h-full flex pb-20 gap-10">
         <div className="w-[20%] lgl:w-[25%] hidden mdl:inline-flex h-full">
-          <ShopSideNav />
+          <ShopSideNav
+            onCategorySelect={handleCategorySelect}
+            onBrandSelect={handleBrandSelect}
+          />
         </div>
         <div className="w-full mdl:w-[80%] lgl:w-[75%] h-full flex flex-col gap-10">
-          <ProductBanner itemsPerPageFromBanner={itemsPerPageFromBanner} />
-          <Pagination itemsPerPage={itemsPerPage} />
+          <Pagination
+            itemsPerPage={itemsPerPage}
+            selectedCategory={selectedCategory}
+            selectedBrand={selectedBrand}
+            onItemsPerPageChange={(items) => setItemsPerPage(items)}
+          />
         </div>
       </div>
       {/* ================= Products End here ===================== */}
